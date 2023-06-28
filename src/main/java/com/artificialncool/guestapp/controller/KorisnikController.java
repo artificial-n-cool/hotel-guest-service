63408fa5c59536a7e78e5620f1b8d332775b2610
njmarko <marko.njegomir@uns.ac.rs>
2023-06-28 19:06:52 +0200

    feat(find_user_by_id)!: implemented finding of user by id

--- src/main/java/com/artificialncool/guestapp/controller/KorisnikController.java
+++ src/main/java/com/artificialncool/guestapp/controller/KorisnikController.java
@@ -3,12 +3,14 @@ package com.artificialncool.guestapp.controller;
 import com.artificialncool.guestapp.dto.model.HostDTO;
 import com.artificialncool.guestapp.dto.model.HostRequestDTO;
 import com.artificialncool.guestapp.service.KorisnikService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -22,6 +24,19 @@ public class KorisnikController {
         this.korisnikService = korisnikService;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<HostDTO> readOne(@PathVariable String id) {
+
+        try {
+            return new ResponseEntity<>(
+                    korisnikService.toDTO(korisnikService.getById(id)),
+                    HttpStatus.OK
+            );
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nema korisnika sa tim ID", e);
+        }
+    }
 
     @GetMapping(value = "/search-hosts")
     @ResponseStatus(HttpStatus.OK)
